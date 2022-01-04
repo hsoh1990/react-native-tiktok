e9bd34299fe6cc6c2e3c991f18ba4ef6826e8aab
wellstoneoh <ohs4123@gmail.com>
2022-01-04 13:18:36 +0900

    기본 문법

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+import splash from './assets/splash.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{color: '#888', fontSize: 18}}>
+        Open up App.js to start working on your app!
+      </Text>
+
+      {/*<Image source={require('./assets/splash.png')} style={{width: 500, height: 200}}/>*/}
+      <Image source={splash} style={{width: 500, height: 200}}/>
+      {/*<Image source={splash} style={styles.splash}/>*/}
+
+
+      <Image source={{uri: "https://pbs.twimg.com/profile_images/1148616067/phph86hLS_400x400"}} style={{width: 200, height: 200}}/>
+
+      <TouchableOpacity
+        onPress={() => alert('Hello, world!')}
+        style={{ backgroundColor: 'blue' }}>
+        <Text style={{ fontSize: 20, color: '#fff' }}>Pick a photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,4 +31,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  splash: {
+    width: 500,
+    height: 200
+  }
 });
