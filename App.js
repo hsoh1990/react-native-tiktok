1d492a58caafc154aea2c6ab56d938aa58cb4efd
wellstoneoh <ohs4123@gmail.com>
2022-01-17 10:43:16 +0900

    create app

--- App.js
+++ App.js
@@ -1,25 +1,9 @@
-import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
-import splash from './assets/splash.png';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{color: '#888', fontSize: 18}}>
-        Open up App.js to start working on your app!
-      </Text>
-
-      {/*<Image source={require('./assets/splash.png')} style={{width: 500, height: 200}}/>*/}
-      <Image source={splash} style={{width: 500, height: 200}}/>
-      {/*<Image source={splash} style={styles.splash}/>*/}
-
-
-      <Image source={{uri: "https://pbs.twimg.com/profile_images/1148616067/phph86hLS_400x400"}} style={{width: 200, height: 200}}/>
-
-      <TouchableOpacity
-        onPress={() => alert('Hello, world!')}
-        style={{ backgroundColor: 'blue' }}>
-        <Text style={{ fontSize: 20, color: '#fff' }}>Pick a photo</Text>
-      </TouchableOpacity>
+      <Text>To share a photo from your phone with a friend, just press the button below!</Text>
     </View>
   );
 }
@@ -31,9 +15,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-
-  splash: {
-    width: 500,
-    height: 200
-  }
 });
